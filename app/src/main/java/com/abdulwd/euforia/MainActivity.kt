c4171e6134cf929b80fe7760af3c502545d87e74
Abdul Wadood <abdulwd97@gmail.com>
2017-09-04 19:48:27 +0530

    Add items to navigation drawer.

--- app/src/main/java/com/abdulwd/euforia/MainActivity.kt
+++ app/src/main/java/com/abdulwd/euforia/MainActivity.kt
@@ -25,7 +25,6 @@ import android.support.design.widget.Snackbar
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.drawer_layout
 import kotlinx.android.synthetic.main.activity_main.nav_view
@@ -60,41 +59,25 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
-
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
+            R.id.nav_library -> {
                 // Handle the camera action
             }
-            R.id.nav_gallery -> {
+            R.id.nav_playlists -> {
 
             }
-            R.id.nav_slideshow -> {
+            R.id.nav_queue -> {
 
             }
-            R.id.nav_manage -> {
+            R.id.nav_folders -> {
 
             }
-            R.id.nav_share -> {
+            R.id.nav_settings -> {
 
             }
-            R.id.nav_send -> {
+            R.id.nav_help_and_feedback -> {
 
             }
         }
